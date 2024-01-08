0c81728861d74abe92fc83a2a85bfb647aad8ce6
Владислав Зобнин <v.zobnin@avto.pro>
2024-01-08 19:15:13 +0200

    Добавит валидацию для формы регистрации

--- front/src/components/ModalRegistration.tsx
+++ front/src/components/ModalRegistration.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Field, Form, Formik } from "formik";
+
 import {
     Modal,
     ModalOverlay,
@@ -9,9 +11,23 @@ import {
     ModalCloseButton,
     Button,
     useDisclosure,
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    Input,
 } from "@chakra-ui/react";
 
 export const ModalRegistration = () => {
+    function validateName(value: any) {
+        let error;
+        if (!value) {
+            error = "Name is required";
+        } else if (value.toLowerCase() !== "naruto") {
+            error = "Jeez! You're not a fan 😱🎃";
+        }
+        return error;
+    }
+
     const { isOpen, onOpen, onClose } = useDisclosure();
     return (
         <>
@@ -22,7 +38,57 @@ export const ModalRegistration = () => {
                 <ModalContent className="modal">
                     <ModalHeader>Modal Title</ModalHeader>
                     <ModalCloseButton />
-                    <ModalBody>Some text</ModalBody>
+                    <ModalBody>
+                        <Formik
+                            initialValues={{ name: "Sasuke" }}
+                            onSubmit={(values: any, actions: any) => {
+                                setTimeout(() => {
+                                    alert(JSON.stringify(values, null, 2));
+                                    actions.setSubmitting(false);
+                                }, 1000);
+                            }}
+                        >
+                            {(props: any) => (
+                                <Form>
+                                    <Field name="name" validate={validateName}>
+                                        {({
+                                            field,
+                                            form,
+                                        }: {
+                                            field: any;
+                                            form: any;
+                                        }) => (
+                                            <FormControl
+                                                isInvalid={
+                                                    form.errors.name &&
+                                                    form.touched.name
+                                                }
+                                            >
+                                                <FormLabel>
+                                                    First name
+                                                </FormLabel>
+                                                <Input
+                                                    {...field}
+                                                    placeholder="name"
+                                                />
+                                                <FormErrorMessage>
+                                                    {form.errors.name}
+                                                </FormErrorMessage>
+                                            </FormControl>
+                                        )}
+                                    </Field>
+                                    <Button
+                                        mt={4}
+                                        colorScheme="teal"
+                                        isLoading={props.isSubmitting}
+                                        type="submit"
+                                    >
+                                        Submit
+                                    </Button>
+                                </Form>
+                            )}
+                        </Formik>
+                    </ModalBody>
 
                     <ModalFooter>
                         <Button colorScheme="blue" mr={3} onClick={onClose}>
