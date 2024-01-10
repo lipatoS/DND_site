6db02b5f992b764ff908f636076f68aad6eabd3a
LipatoS <sasharubaiko@gmail.com>
2024-01-11 01:49:14 +0200

    забыл апи тестируем в 2 часа ночи

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from accounts.activate_account import activate_account
+from accounts.views import RegistrationView, LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # API регистрации users
+    path('api/registration', RegistrationView.as_view(), name='registration'),
+    path('api/login', LoginView.as_view(), name='login'),
     path('activate/<str:activation_token>/', activate_account, name='activate_account'),
 ]
