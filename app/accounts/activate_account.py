967c7e568a5b459d7894ecd7d36113627489cfc6
LipatoS <sasharubaiko@gmail.com>
2024-01-11 20:05:06 +0200

    тест редиректа активации

--- app/accounts/activate_account.py
+++ app/accounts/activate_account.py
@@ -9,23 +9,11 @@ CustomUser = get_user_model()
 
 def activate_account(activation_token):
     try:
-        # Найдите пользователя с указанным ключом активации
         user = CustomUser.objects.get(activation_token=activation_token)
-
-        # Установите is_authenticated в True, очистите activation_token и сохраните пользователя
         user.is_authenticated = True
         user.activation_token = ''
         user.save()
 
-        # Возвращайте сообщение об успешной активации или что-то еще
-        # return HttpResponse("Учетная запись успешно активирована")
-        return redirect(f'{settings.SITE_URL}/activation-success')
-        # return Response({'message': 'Учетная запись успешно активирована', 'response': status.HTTP_200_OK},
-        #                 status=status.HTTP_200_OK)
-
+        return redirect('http://localhost:3000/activation-success')
     except ObjectDoesNotExist:
-        # Если пользователь с указанным ключом активации не найден, возвращайте сообщение об ошибке
-        # return HttpResponse("Ошибка: Учетная запись не найдена")
-        return redirect(f'{settings.SITE_URL}/page-not-found')
-        # return Response({'message': 'Учетная запись не найдена', 'response': status.HTTP_400_BAD_REQUEST},
-        #                 status=status.HTTP_400_BAD_REQUEST)
+        return redirect('http://localhost:3000/page-not-found')
